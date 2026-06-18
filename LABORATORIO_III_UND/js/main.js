d7ebe3c8adec3f844fa78e8612b473a5ec6014f8
ROLANDO PAJARES ROJAS <7558911642@untrm.edu.pe>
2026-06-18 10:07:08 -0500

    Fase 2: Variables y tipos de datos

--- LABORATORIO_III_UND/js/main.js
+++ LABORATORIO_III_UND/js/main.js
@@ -4,3 +4,17 @@
 console.log("Iniciando laboratorio...");
 
 
+// FASE 2: Variables y Literales Primitivos
+
+const empresa = "JSLab";
+
+let edad = 20;
+let esEstudiante = true;
+
+console.log("Empresa:", empresa);
+console.log("Edad:", edad);
+console.log("Tipo de edad:", typeof edad);
+
+console.log("¿Es estudiante?:", esEstudiante);
+console.log("Tipo de esEstudiante:", typeof esEstudiante);
+
